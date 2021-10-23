a178ef294ed29b23d50c7cfa41d29bcfe0d01502
ConorDunne <conoredunne@gmail.com>
2021-10-23 19:00:38 +0100

    Created initial menu layout

--- src/ClubDoorManager.py
+++ src/ClubDoorManager.py
@@ -1,6 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import tkinter as tk
+from tkinter import *
 
-root = tk.Tk()
-root.mainloop()
+top = Tk()
+top.geometry("400x250")
+
+searchFrame = Frame(top)  
+searchFrame.pack()
+
+search = Label(searchFrame,text = "Member ID").grid(row = 0, column = 0)  
+memberID = Entry(searchFrame).grid(row = 0, column = 1)
+memberSearch = Button(searchFrame, text = "Search").grid(row = 0, column = 2)
+
+manageFrame = Frame(top)
+manageFrame.pack()
+
+newMember = Button(manageFrame, text = "New Member").grid(row = 1, column = 0)
+barcode = Button(manageFrame, text = "Barcode Reader").grid(row = 1, column = 1)
+
+
+top.mainloop()
