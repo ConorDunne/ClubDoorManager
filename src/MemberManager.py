5a1893b6c1ea87a709b8240e2726a46558da714f
ConorDunne <conoredunne@gmail.com>
2021-10-25 21:16:34 +0100

    Users can be modified and signed in

--- src/MemberManager.py
+++ src/MemberManager.py
@@ -1,13 +1,21 @@
 import tkinter
 from tkinter import scrolledtext
+from tkinter import messagebox
 from Member import Member
+from datetime import date
 
 class MemberManager(tkinter.Toplevel):
 	member = None
 
+	memberComment = None
+	membershipPaid = None
+	signedIn = None
+
 	def __init__(self, member, master = None):
 		super().__init__(master = master)
 		self.member = member
+		self.membershipPaid = self.member.membershipPaid
+		self.signedIn = False
 		self.createWindow()
 
 	def createWindow(self):
@@ -31,22 +39,41 @@ class MemberManager(tkinter.Toplevel):
 		attendFrame = tkinter.Frame(self)
 		attendFrame.pack()
 
-		attendButton = tkinter.Button(attendFrame, text = "Sign in")
+		attendButton = tkinter.Button(attendFrame, text = "Sign in", command = self.memberSignedIn)
 		attendButton.grid(row = 0, column = 0)
 		if not self.member.membershipPaid:
-			membershipPayment = tkinter.Button(attendFrame, text = "Pay Membership")
+			membershipPayment = tkinter.Button(attendFrame, text = "Pay Membership", command = self.membershipPaymentMade)
 			membershipPayment.grid(row = 0, column = 1)
 
 
 		commentFrame = tkinter.LabelFrame(self, text="Member Comments")
 		commentFrame.pack(fill="both", expand="yes")
 
-		memberComment = scrolledtext.ScrolledText(commentFrame, wrap=tkinter.WORD)
-		memberComment.insert(tkinter.END, self.member.comment)
-		memberComment.pack()
+		self.memberComment = scrolledtext.ScrolledText(commentFrame, wrap=tkinter.WORD)
+		self.memberComment.insert(tkinter.END, self.member.comment)
+		self.memberComment.pack()
 
-		save = tkinter.Button(self, text = "Save")
+		save = tkinter.Button(self, text = "Save", command = self.saveMember)
 		save.pack()
+	
+	def memberSignedIn(self):
+		self.signedIn = True
+	
+	def membershipPaymentMade(self):
+		self.membershipPaid = True
+
+	def saveMember(self):
+		self.member.comment = self.memberComment.get("1.0", tkinter.END)
+		self.member.membershipPaid = self.membershipPaid
+
+		if self.signedIn:
+			today = str(date.today())
+			if today not in self.member.attended:
+				self.member.attended.append(today)
+			else:
+				messagebox.showinfo("Error","User already logged in today")  
+
+		self.onExit()
 
 	def onExit(self):
-		self.master.destroy()
+		self.destroy()
