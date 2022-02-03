51dc493382832e2caf33bce4c27ae2b4c5c95959
ConorDunne <conoredunne@gmail.com>
2022-02-03 21:50:46 +0000

    Can read UID from NFC Card

--- src/MemberCreator.py
+++ src/MemberCreator.py
@@ -1,6 +1,8 @@
 import tkinter
 from tkinter import scrolledtext
+
 from Member import Member
+from NFC_Reader import ReadCard
 
 class MemberCreator(tkinter.Toplevel):
 	members = None
@@ -9,15 +11,17 @@ class MemberCreator(tkinter.Toplevel):
 	nameEntry = None
 	isMemberPaid = None
 	memberComment = None
+	card = None
 
 	def __init__(self, memberId, members, master = None):
 		super().__init__(master = master)
 		self.members = members
 		self.memberId = memberId
+		self.card = []
 		self.createWindow()
 
 	def createWindow(self):
-		self.geometry("500x500")
+		self.geometry("500x600")
 		self.title("Club Door Manager - Member Creator")
 
 		personalFrame = tkinter.Frame(self)
@@ -28,10 +32,12 @@ class MemberCreator(tkinter.Toplevel):
 		self.nameEntry = tkinter.Entry(personalFrame)
 		self.isMemberPaid = tkinter.BooleanVar()
 		self.memberPaid = tkinter.Checkbutton(personalFrame, text = "Membership Paid", variable = self.isMemberPaid, onvalue = True, offvalue = False)
+		self.assignCard = tkinter.Button(personalFrame, text = "Assign Card", command = self.assignMembershipCard)
 		idLabel.pack()
 		nameLabel.pack()
 		self.nameEntry.pack()
 		self.memberPaid.pack()
+		self.assignCard.pack()		
 
 
 		commentFrame = tkinter.LabelFrame(self, text="Member Comments")
@@ -49,10 +55,16 @@ class MemberCreator(tkinter.Toplevel):
 		info["membershipPaid"] = self.isMemberPaid.get()
 		if self.memberComment != "":
 			info["comment"] = self.memberComment.get("1.0", tkinter.END)
+		info["card"] = self.card
 
 		m = Member(self.memberId, info)
 		self.members.append(m)
 		self.onExit()
+
+	def assignMembershipCard(self):
+		card = ReadCard()
+		print(card)
+		self.card.append(card)
 
 	def onExit(self):
 		self.destroy()
